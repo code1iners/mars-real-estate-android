7acb24116b353553abfbe99f2a7cb12504ae9d51
Codeliner <codeliner96@gmail.com>
2021-02-19 18:55:43 +0900

    Converted string value(price, type) in DetailFragment

--- app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
@@ -17,12 +17,10 @@
 package com.example.android.marsrealestate.detail
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.example.android.marsrealestate.detail.DetailFragment
 import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.R
 
 /**
  * The [ViewModel] that is associated with the [DetailFragment].
@@ -39,4 +37,18 @@ class DetailViewModel(@Suppress("UNUSED_PARAMETER")marsProperty: MarsProperty, a
         _selectedProperty.value = marsProperty
     }
 
+    // note. Add displayPropertyPrice and displayPropertyType LiveData Transformations.map
+    val displayPropertyPrice = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(when (it.isRental) {
+            true -> R.string.display_price_monthly_rental
+            false -> R.string.display_price
+        }, it.price)
+    }
+
+    val displayPropertyType = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(R.string.display_type, app.applicationContext.getString(when (it.isRental) {
+            true -> R.string.type_rent
+            false -> R.string.type_sale
+        }))
+    }
 }
