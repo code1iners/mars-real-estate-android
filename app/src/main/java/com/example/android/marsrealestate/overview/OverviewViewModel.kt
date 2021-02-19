b29cc1de63853b06e54eb7f886f6d8f72c13e00c
Codeliner <codeliner96@gmail.com>
2021-02-19 17:23:00 +0900

    Show first item image in OverviewFragment using by Glide

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -36,11 +36,15 @@ import retrofit2.Response
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<String>()
 
     // The external immutable LiveData for the request status String
     val response: LiveData<String>
-        get() = _response
+        get() = _status
+
+    val _property = MutableLiveData<MarsProperty>()
+    val property: LiveData<MarsProperty>
+        get() = _property
 
     // note. Create coroutine job and a CoroutineScope using the main dispatcher
     private var viewModelJob = Job()
@@ -63,9 +67,13 @@ class OverviewViewModel : ViewModel() {
             var getPropertiesDeferred =  MarsApi.retrofitService.getProperties()
             try {
                 var listResult = getPropertiesDeferred.await()
-                _response.value = "Sucess: ${listResult.size} Mars properties retrieved."
+//                _status.value = "Sucess: ${listResult.size} Mars properties retrieved."
+                if (listResult.size > 0) {
+                    _property.value = listResult[0]
+                }
+
             } catch (t: Throwable) {
-                _response.value = "Failure ${t.message}."
+                _status.value = "Failure ${t.message}."
             }
         }
     }
